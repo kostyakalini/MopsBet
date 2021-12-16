327ca4bf363519a4f889da3e24715495d824c9d8
babushka334 <59337417+babushka334@users.noreply.github.com>
2021-12-16 22:26:37 +0300

    Update PasswordGenerate.java

--- src/test/java/by/mops/bet/PasswordGenerate.java
+++ src/test/java/by/mops/bet/PasswordGenerate.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordGenerate {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "kalini.kostya";
+        String rawPassword = "1111";
         String encodedPassword = encoder.encode(rawPassword);
         System.out.println(encodedPassword);
     }
